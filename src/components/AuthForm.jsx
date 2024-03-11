67b021f4967dd7f3879cd914cb5d6d4f654bb2c8
Tim <timilon10271@gmail.com>
2024-03-11 15:00:33 +0500

    fix

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -46,7 +46,7 @@ export default function AuthForm() {
                     rules={[
                         {
                             required: true,
-                            message: 'Please input your username!',
+                            message: 'Please input your email!',
                         },
                         {
                             type: 'email',
